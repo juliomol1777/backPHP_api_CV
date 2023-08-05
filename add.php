97cd8a4ca5d3811ff7267faa7d6942233075a1f5
Julio M <juliomolina17@gmail.com>
2023-08-05 12:40:56 -0300

    primero

---
+++ add.php
@@ -0,0 +1,26 @@
+<?php
+
+    include_once 'apidatos.php';
+    
+    $api = new ApiDatos();
+    $nombre = '';
+    $error = '';
+
+    if(isset($_POST['nombre']) && isset($_FILES['imagen'])){
+
+        if($api->subirImagen($_FILES['imagen'])){
+            $item = array(
+                'nombre' => $_POST['nombre'],
+                'imagen' => $api->getImagen()
+            );
+            //$api->add($item);
+        }else{
+            $api->error('Error con el archivo: ' . $api->getError());
+        }
+    }else{
+        $api->error('Error al llamar a la API');
+    }
+
+
+    
+?>
